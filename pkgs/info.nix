e2176496ab406f92f2c30a27b6f598ec35b8335a
Martijn Boers <martijn@boers.email>
2026-06-08 23:44:41 +0200

    Update info website

--- pkgs/info.nix
+++ pkgs/info.nix
@@ -8,8 +8,8 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchFromRadicle {
     seed = "seed.boers.email";
     repo = "z2r9euHZW161kfQNxdF4apHddD3mm";
-    rev = "490bb24bef80f8e2550071dcbeb39bde837ab50b";
-    hash = "sha256-sXXg4z6JjsR+uTuCGEMOht0k+xuwziE66wFvaG/4aS8=";
+    rev = "936c086d7436e797b1c0d0badf01bd3d5ff20c04";
+    hash = "sha256-Q42RFCgnDlGaXRAg/RSD4rnhjYYDWs2MT5EoQmqo/z0=";
   };
   installPhase = "cp -r . $out";
 }
